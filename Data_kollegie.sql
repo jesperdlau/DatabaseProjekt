b1e55c157841fb3da78359f3c514fc47eb4735a1
jesperdlau <jesperdlau@gmail.com>
2023-03-28 10:53:47 +0200

    Populate DB

---
+++ Data_kollegie.sql
@@ -0,0 +1,99 @@
+-- Data til Kollegiet
+
+
+
+INSERT Resident VALUES
+(00001, 'Ahmed Abdullah', 'ahmed@gmail.com', 10297362, 75634550),
+(81838, 'Spencer Lloyd', 'werwe@hellsmoney.com', 54075690, 96941657),
+(81193, 'Alexis Robbins', 'alekseu6@suksesboss.com', 89651583, 66192438),
+(77612, 'Luther Gibbons', 'nbyrd568@allfrree.xyz', 78033372, 30771149),
+(09208, 'Big Man Shaq', 'babyj23@gcantikored.pw', 96509093, 35913717),
+(59301, 'Moe Lester', 'scottvw1@hulas.us', 01685459, 55353104),
+(80033, 'Chris P. Bacon', 'cedjoe@sonophon.ru', 74142804, 41359985),
+(64910, 'Ben Dover', 'shainurovaelena@bukanimers.com', 82307954, 16744280),
+(36593, 'Torben Torp', 'hornyboy19474@boranora.com', 51239167, 32669505),
+(30777, 'Lars Larsen Muhammed', 'gmfam@menuyul.club', 28653373, 98560359),
+(76179, 'Malthe BR. Esler', 'lillepik@bigboysclub.dk', 25810273, 67446187),
+(93194, 'Juliet Fernandez', 'giomelos@menuyul.club', 19756075, 03272464),
+(27183, 'Laila Andersen', 'pfqlug9yix@clk2020.info', 63800239, 86397684),
+(21827, 'Sugma Kok', 'bulikina@bukanimers.com', 52978252, 78760567),
+(52923, 'Dean Mohr', 'azamyt@greendike.com', 26480915, 80488983),
+(40938, 'Frida Torp', 'frida@torp.family.dk', 13832969, 95671075),
+(62775, 'Uffe Torp', 'uffe@torp.family.dk', 76340667, 32706604),
+(99798, 'Steffen Torp', 'steffen@torp.family.dk', 72941802, 32399798),
+(03934, 'Johanne Torp', 'johanne@torp.family.dk', 27226786, 65147817),
+(13732, 'Kristine Torp', 'kristine@torp.family.dk', 31243048, 63825321);
+
+
+INSERT ROOMS VALUES
+(01, 1, 'A', False, False),
+(02, 1, 'A', False, False),
+(03, 1, 'A', True, True),
+(04, 1, 'A', False, False),
+(05, 1, 'A', False, False),
+(06, 1, 'A', False, False),
+(07, 1, 'A', False, False),
+(08, 1, 'A', True, True),
+(09, 1, 'A', True, False),
+(10, 1, 'A', False, False),
+(01, 2, 'A', False, False),
+(02, 2, 'A', False, False),
+(03, 2, 'A', True, False),
+(04, 2, 'A', True, False),
+(05, 2, 'A', True, False),
+(06, 2, 'A', True, False),
+(07, 2, 'A', True, False),
+(08, 2, 'A', True, False),
+(09, 2, 'A', True, False),
+(10, 2, 'A', True, False),
+(01, 1, 'B', True, False),
+(02, 1, 'B', True, False),
+(03, 1, 'B', True, False),
+(04, 1, 'B', True, False),
+(05, 1, 'B', True, False),
+(01, 2, 'B', True, False),
+(02, 2, 'B', True, False),
+(03, 2, 'B', True, False),
+(04, 2, 'B', True, True),
+(05, 2, 'B', True, False);
+
+
+
+INSERT Lives VALUES
+(00001, 03, 1, 'A'),
+(81838, 02, 2, 'A'),
+(81193, 04, 2, 'B'),
+(77612, 05, 2, 'B'),
+(09208, 06, 2, 'A'),
+(59301, 08, 1, 'A'),
+(80033, 10, 2, 'A'),
+(64910, 03, 1, 'B'),
+(36593, 09, 1, 'A'),
+(30777, 03, 1, 'A'),
+(76179, 05, 2, 'B'),
+(93194, 01, 1, 'B'),
+(27183, 02, 1, 'B'),
+(21827, 07, 2, 'A'),
+(52923, 06, 2, 'A'),
+(40938, 05, 2, 'A'),
+(62775, 08, 1, 'A'),
+(99798, 04, 2, 'B'),
+(03934, 03, 2, 'B'),
+(13732, 03, 2, 'A');
+
+
+INSERT Laundry VALUES
+    ('L01', 'Vask', False),
+    ('L02', 'Vask', False),
+    ('L03', 'Tørre', False),
+    ('L04', 'Tørre', True);
+
+
+INSERT Boats VALUES
+    ('B01', 'W.T. Frigg', 'Kano', False),
+    ('B02', 'Malthe Boatsler', 'Kano', True),
+    ('B03', 'Bo(at)', 'Kano', False),
+    ('B04', 'Bloat', 'Kajak', False);
+
+
+
